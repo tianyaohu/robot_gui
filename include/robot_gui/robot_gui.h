ffb0ac5e484b0970f5ec88523709ae6828775d0e
Frank Hu <tianyaoh@hotmail.com>
2023-12-08 03:21:15 +0000

    Velocity UI added

--- include/robot_gui/robot_gui.h
+++ include/robot_gui/robot_gui.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ros/ros.h>
+#include <sstream>
 
 #define CVUI_IMPLEMENTATION
 #include "robot_gui/cvui.h"
@@ -13,38 +14,12 @@ public:
   // constructor
   RobotGUI()
       : vec_info({"Start1", "Start2", "Start2", "Start2", "Start2", "Start2",
-                  "Start2", "Start2", "Start2", "Start2", "Start2"}) {
+                  "Start2", "Start2", "Start2", "Start2", "Start2"}),
+        linear_x(0), angular_z(0) {
     cout << "RobotGUI Constructor is called" << endl;
   };
   // destructor
   ~RobotGUI() { cout << "RobotGUI Destructor is called" << endl; };
-
-  // init UI frame
-  void init_UI_frame(int ui_height, int ui_width) {
-    this->ui_height = ui_height;
-    this->ui_width = ui_width;
-    this->frame = cv::Mat(this->ui_height, this->ui_width, CV_8UC3);
-  }
-
-  // General Info
-  void InfoUI(int padding) {
-    // init needed x,y offset
-    int x_offset = 5, line_space = 5, line_height = 15 + line_space;
-
-    int window_width = ui_width - padding * 2;
-    int window_height = (this->vec_info.size() + 1) * line_height +
-                        5; // 5 is for the first half of line space
-    //  the following should house info_topic string
-    cvui::window(this->frame, this->pos.x, this->pos.y, window_width,
-                 window_height, "info_topic_name");
-    // adding half of line space
-    this->pos.y += 5;
-    // write all info lines;
-    for (const std::string &row : vec_info) {
-      this->pos.y += line_height; // 15 is standard text height
-      cvui::text(this->frame, this->pos.x + x_offset, this->pos.y, row);
-    }
-  }
 
   void DisplayUI() {
     cout << "DisplayUI called" << endl;
@@ -64,6 +39,15 @@ public:
 
       // info ui
       this->InfoUI(padding);
+
+      // add padding
+      this->pos.y += 30;
+
+      // control section
+      // pass
+
+      // velocity section
+      this->VelUI(padding);
 
       // update ui and show window
       cvui::update();
@@ -103,4 +87,64 @@ private:
   // init point for ui alignment
   cv::Point pos;
   // section dimensions
+
+  // init UI frame
+  void init_UI_frame(int ui_height, int ui_width) {
+    this->ui_height = ui_height;
+    this->ui_width = ui_width;
+    this->frame = cv::Mat(this->ui_height, this->ui_width, CV_8UC3);
+  }
+
+  // General Info
+  void InfoUI(int width_padding) {
+    // init needed x,y offset
+    int x_offset = 5, line_space = 5, line_height = 15 + line_space;
+
+    int window_width = ui_width - width_padding * 2;
+    int window_height = (this->vec_info.size() + 1) * line_height +
+                        5; // 5 is for the first half of line space
+    //  the following should house info_topic string
+    cvui::window(this->frame, this->pos.x, this->pos.y, window_width,
+                 window_height, "info_topic_name");
+    // adding half of line space
+    this->pos.y += 5;
+    // write all info lines;
+    for (const std::string &row : vec_info) {
+      this->pos.y += line_height; // 15 is standard text height
+      cvui::text(this->frame, this->pos.x + x_offset, this->pos.y, row);
+    }
+  }
+
+  void VelUI(int width_padding) {
+    int single_window_width = (this->ui_width - width_padding * 3) / 2;
+    int h = 45; // window title is 20, normal text height is 15
+
+    // text starting point
+    int title_height = 25;
+    int text_start = single_window_width / 3;
+
+    // convert a floating number to string with 2 decimal precision rounding
+    auto float2String = [](float f) {
+      std::ostringstream oss;
+      oss << std::fixed << std::setprecision(2) << f;
+      return oss.str();
+    };
+    // linear vel window
+    cvui::window(this->frame, this->pos.x, this->pos.y, single_window_width, h,
+                 "Linear velocity:");
+    // fill linear value to window
+    cvui::text(this->frame, this->pos.x + text_start,
+               this->pos.y + title_height,
+               float2String(this->linear_x) + " m/sec", 0.4, 0xff0000);
+
+    // angular vel window
+    cvui::window(this->frame, this->pos.x + single_window_width + width_padding,
+                 this->pos.y, single_window_width, h, "Angular velocity:");
+    cvui::text(this->frame,
+               this->pos.x + text_start + width_padding + single_window_width,
+               this->pos.y + title_height,
+               float2String(this->linear_x) + " rod/sec", 0.4, 0xff0000);
+  }
+
+  // Velocity Info
 };
